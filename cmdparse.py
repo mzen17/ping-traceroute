ddc6f75762cd111a309dca04440e3daca5677e79
Mike Zeng <mzen17.jp@gmail.com>
2023-10-12 16:09:01 -0500

    Refactoring and renaming to make assignment fit to the requirements of assignment

--- cmdparse.py
+++ cmdparse.py
@@ -5,15 +5,15 @@ import argparse
 
 def ping_args() -> argparse.Namespace:
     argParser = argparse.ArgumentParser()
-    argParser.add_argument("-c", "--ping-count", help="Amount of pings to send.")
-    argParser.add_argument("-d", "--destination", help="Destination to ICMP packets, pings or traceroute.")
+    argParser.add_argument("-n", "--ping-count", help="Amount of pings to send.")
+    argParser.add_argument("-destination", "--destination", help="Destination to ICMP packets, pings or traceroute.")
     argParser.add_argument("-ttl", "--time-to-live", help="Time-To-Live of ping.")
     args = argParser.parse_args()
     return args
 
 def traceroute_args() -> argparse.Namespace:
     argParser = argparse.ArgumentParser()
-    argParser.add_argument("-d", "--destination", help="Destination to ICMP packets, pings or traceroute.")
+    argParser.add_argument("-destination", "--destination", help="Destination to ICMP packets, pings or traceroute.")
     argParser.add_argument("-n_hops", "--number-of-hops", help="Maximum number of hops for tracing.")
     args = argParser.parse_args()
     return args
